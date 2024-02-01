b2b5ebad70591f3dda1c5b5bd4947d719bb6714a
tbala2805 <tbala2805@gmail.com>
2024-02-01 18:44:59 +0530

    for QA

--- pythonsample.py
+++ pythonsample.py
@@ -1,6 +1,7 @@
 print("Hi there!")
 
 def samplefucntion():
-    print('hi there!')
+    print('hi there! this is bala')
+    print(' print this while move from qa to production')
 
 samplefucntion()
